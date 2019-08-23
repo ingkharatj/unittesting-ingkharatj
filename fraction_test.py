7888d1b78ff0dc0c6052d65f1a3d593a23461257
Ingkharat <ingkharat.j@ku.th>
2019-08-23 18:20:49 +0700

    almost finish

--- fraction_test.py
+++ fraction_test.py
@@ -25,13 +25,54 @@ class FractionTest(unittest.TestCase):
         # Constructor should provide default denominator = 1
         f = Fraction(99)
         self.assertEqual("99", f.__str__())
+    
+    def test__init__(self):
+        f = Fraction(0)
+        self.assertEqual(0,f.numerator)
+        self.assertEqual(1,f.denominator)
 
+        f = Fraction(1,0)
+        self.assertEqual(1,f.numerator)
+        self.assertEqual(0,f.denominator)
+
+        f = Fraction(1,2)
+        self.assertEqual(1,f.numerator)
+        self.assertEqual(2,f.denominator)
+
+        f = Fraction(4,8)
+        self.assertEqual(1,f.numerator)
+        self.assertEqual(2,f.denominator)
+
+        f = Fraction(23,13)
+        self.assertEqual(23,f.numerator)
+        self.assertEqual(13,f.denominator)
+
+        f = Fraction(-7)
+        self.assertEqual(-7,f.numerator)
+        self.assertEqual(1,f.denominator)
+        
+        f = Fraction(3,-8)
+        self.assertEqual(3,f.numerator)
+        self.assertEqual(-8,f.denominator)
+
+        f = Fraction(-5,6)
+        self.assertEqual(-5,f.numerator)
+        self.assertEqual(6,f.denominator)
+
+        f = Fraction(-5,10)
+        self.assertEqual(-1,f.numerator)
+        self.assertEqual(2,f.denominator)
+
+
+        
     # TODO Write tests for __init__, __eq__, +, *.
     # Here is an example, but you must add more test cases.  
     # The test requires that your __eq__ is correct.
     def test_add(self):
         # 3/4 = 2/3 + 1/12
         self.assertEqual(Fraction(3,4), Fraction(1,12)+Fraction(2,3))
+        self.assertEqual(Fraction(5,9), Fraction(4,9)+Fraction(1,9))
+        self.assertEqual(Fraction(3,2),Fraction(1)+Fraction(1,2))
 
     def test_eq(self):
         f = Fraction(1,2)
